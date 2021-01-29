41e32a96f2e92b4e2e83237fa977bc45d280f4e0
song <johyesong8686@gmail.com>
2021-01-30 01:44:45 +0900

    fix(back): 로그인 로그아웃 세션 에러 수정

--- back/router/user.js
+++ back/router/user.js
@@ -2,32 +2,35 @@ const express = require('express');
 const { User } = require('../models');
 router = express.Router();
 const dotenv = require('dotenv');
-const session = require('express-session');
 dotenv.config();
 
 router.post('/login', async (req, res) => {
-  const findResult = await User.findOne({
-    where: { loginId: req.body.loginId },
-  });
-  if (!findResult) {
-    const createResult = await User.create({
-      loginId: req.body.loginId,
-      name: req.body.name,
-      email: req.body.email,
-      img: req.body.img,
-      logoUrl: req.body.logoUrl,
-      option: req.body.option,
-    });
-
-    return res.send(createResult);
+  if (req.session.userId) {
+    return res.status(401).send({ error: 'Alredy Login' });
   } else {
-    return res.send(findResult);
+    req.session.userId = req.body.loginId;
+    const findResult = await User.findOne({
+      where: { loginId: req.body.loginId },
+    });
+    if (!findResult) {
+      const createResult = await User.create({
+        loginId: req.body.loginId,
+        name: req.body.name,
+        email: req.body.email,
+        img: req.body.img,
+        logoUrl: req.body.logoUrl,
+        option: req.body.option,
+      });
+
+      return res.send(createResult);
+    } else {
+      return res.send(findResult);
+    }
   }
 });
 
 router.post('/logout', (req, res) => {
-  if (req.session) {
-    console.log(session);
+  if (req.session.userId) {
     req.session.destroy();
     return res.status(201).send();
   } else {
